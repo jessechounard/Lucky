a098439acf413bb672cb1f48e4abfa8d8073faa3
Jesse Chounard <jessechounard@gmail.com>
2025-01-06 19:45:30 -0600

    Updated examples to match library updates

--- Source/Examples/03-TextureAtlas.cpp
+++ Source/Examples/03-TextureAtlas.cpp
@@ -1,123 +1,104 @@
 #include <memory>
-#include <stdint.h>
 
-#define _CRTDBG_MAP_ALLOC
-#include <crtdbg.h>
-
-#define SDL_MAIN_USE_CALLBACKS
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
-#include <spdlog/spdlog.h>
-
-#include <Lucky/Graphics/Color.hpp>
+#define LUCKY_MAIN
 #include <Lucky/Graphics/BatchRenderer.hpp>
-#include <Lucky/Graphics/GraphicsDevice.hpp>
 #include <Lucky/Graphics/Texture.hpp>
 #include <Lucky/Graphics/TextureAtlas.hpp>
+#include <Lucky/LuckyMain.hpp>
 #include <Lucky/Utility/FileSystem.hpp>
 
-const int windowWidth = 1920;
-const int windowHeight = 1080;
-
-SDL_Window *window;
-std::shared_ptr<Lucky::GraphicsDevice> graphicsDevice;
-std::unique_ptr<Lucky::BatchRenderer> batchRenderer;
-std::shared_ptr<Lucky::Texture> texture;
-std::shared_ptr<Lucky::TextureAtlas> textureAtlas;
-uint64_t currentTime;
-
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+struct ExampleData
 {
-    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+    std::unique_ptr<Lucky::BatchRenderer> batchRenderer;
+    std::shared_ptr<Lucky::TextureAtlas> textureAtlas;
+    std::shared_ptr<Lucky::Texture> texture;
+};
 
-    SDL_SetAppMetadata("Texture Atlas Example", "1.0", "dev.jessechounard.textureatlas");
+void SDLCALL LuckyGetConfig(Lucky::ConfigData *configData)
+{
+    configData->screenWidth = 1920;
+    configData->screenHeight = 1080;
+    configData->windowTitle = "Texture Atlas Example";
+}
 
-    if (!SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_GAMEPAD))
-    {
-        spdlog::error("Failed to initialize SDL {}", SDL_GetError());
-        return SDL_APP_FAILURE;
-    }
+SDL_AppResult SDLCALL LuckyInitialize(Lucky::GameData *gameData)
+{
+    ExampleData *exampleData = new ExampleData();
 
-    auto attributes = Lucky::GraphicsDevice::PrepareWindowAttributes(Lucky::GraphicsAPI::OpenGL);
-
-    window = SDL_CreateWindow("TextureAtlas Example", windowWidth, windowHeight, attributes);
-    if (window == NULL)
-    {
-        spdlog::error("Failed to create window {}", SDL_GetError());
-        return SDL_APP_FAILURE;
-    }
-
-    graphicsDevice = std::make_shared<Lucky::GraphicsDevice>(
-        Lucky::GraphicsAPI::OpenGL, window, Lucky::VerticalSyncType::AdaptiveEnabled);
-    batchRenderer = std::make_unique<Lucky::BatchRenderer>(graphicsDevice, 1000);
+    exampleData->batchRenderer = std::make_unique<Lucky::BatchRenderer>(gameData->graphicsDevice, 1000);
 
     const auto atlasPath = "./texture_atlas.json";
-    textureAtlas = std::make_shared<Lucky::TextureAtlas>(atlasPath);
-    const auto texturePath = Lucky::CombinePaths(Lucky::GetPathName(atlasPath), textureAtlas->TexturePath());
-    texture = std::make_shared<Lucky::Texture>(texturePath);
+    exampleData->textureAtlas = std::make_shared<Lucky::TextureAtlas>(atlasPath);
+    const auto texturePath =
+        Lucky::CombinePaths(Lucky::GetPathName(atlasPath), exampleData->textureAtlas->TexturePath());
+    exampleData->texture = std::make_shared<Lucky::Texture>(texturePath);
 
-    currentTime = SDL_GetPerformanceCounter();
+    gameData->userData = exampleData;
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDLCALL LuckyEvent(Lucky::GameData *gameData, SDL_Event *event, bool *eventHandled)
+{
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDLCALL LuckyUpdate(Lucky::GameData *gameData, double deltaSeconds)
+{
+    ExampleData *exampleData = (ExampleData *)gameData->userData;
 
     return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+SDL_AppResult SDLCALL LuckyDraw(Lucky::GameData *gameData)
 {
-    if (event->type == SDL_EVENT_QUIT)
-    {
-        return SDL_APP_SUCCESS;
-    }
-    return SDL_APP_CONTINUE;
-}
+    ExampleData *exampleData = (ExampleData *)gameData->userData;
 
-SDL_AppResult SDL_AppIterate(void *appstate)
-{
-    uint64_t newTime = SDL_GetPerformanceCounter();
-    double frameTime = (newTime - currentTime) / (double)SDL_GetPerformanceFrequency();
-    currentTime = newTime;
+    gameData->graphicsDevice->BeginFrame();
+    gameData->graphicsDevice->ClearScreen(Lucky::Color::CornflowerBlue);
 
-    graphicsDevice->BeginFrame();
-    graphicsDevice->ClearScreen(Lucky::Color::CornflowerBlue);
+    const int width = gameData->graphicsDevice->GetScreenWidth();
+    const int height = gameData->graphicsDevice->GetScreenHeight();
 
-    batchRenderer->Begin(Lucky::BlendMode::Alpha, texture);
+    exampleData->batchRenderer->Begin(Lucky::BlendMode::Alpha, exampleData->texture);
 
-    const auto region1 = textureAtlas->GetRegion("playerShip1_blue.png");
-    batchRenderer->BatchQuad(&region1.bounds, glm::vec2(windowWidth / 4.0f, windowHeight / 4.0f), 0.0f,
+    const auto region1 = exampleData->textureAtlas->GetRegion("playerShip1_blue.png");
+    exampleData->batchRenderer->BatchQuad(&region1.bounds, glm::vec2(width / 4.0f, height / 4.0f), 0.0f,
         glm::vec2(1.0f, 1.0f), region1.originCenter,
         region1.rotated ? Lucky::UVMode::RotatedCW90 : Lucky::UVMode::Normal, Lucky::Color::White);
 
-    const auto region2 = textureAtlas->GetRegion("playerShip2_green.png");
-    batchRenderer->BatchQuad(&region2.bounds, glm::vec2(windowWidth / 4.0f, windowHeight * 3.0f / 4.0f), 0.0f,
+    const auto region2 = exampleData->textureAtlas->GetRegion("playerShip2_green.png");
+    exampleData->batchRenderer->BatchQuad(&region2.bounds, glm::vec2(width / 4.0f, height * 3.0f / 4.0f), 0.0f,
         glm::vec2(1.0f, 1.0f), region2.originCenter,
         region2.rotated ? Lucky::UVMode::RotatedCW90 : Lucky::UVMode::Normal, Lucky::Color::White);
 
-    const auto region3 = textureAtlas->GetRegion("playerShip3_orange.png");
-    batchRenderer->BatchQuad(&region3.bounds, glm::vec2(windowWidth * 3.0f / 4.0f, windowHeight / 4.0f), 0.0f,
+    const auto region3 = exampleData->textureAtlas->GetRegion("playerShip3_orange.png");
+    exampleData->batchRenderer->BatchQuad(&region3.bounds, glm::vec2(width * 3.0f / 4.0f, height / 4.0f), 0.0f,
         glm::vec2(1.0f, 1.0f), region3.originCenter,
         region3.rotated ? Lucky::UVMode::RotatedCW90 : Lucky::UVMode::Normal, Lucky::Color::White);
 
-    const auto region4 = textureAtlas->GetRegion("ufoRed.png");
-    batchRenderer->BatchQuad(&region4.bounds, glm::vec2(windowWidth * 3.0f / 4.0f, windowHeight * 3.0f / 4.0f), 0.0f,
+    const auto region4 = exampleData->textureAtlas->GetRegion("ufoRed.png");
+    exampleData->batchRenderer->BatchQuad(&region4.bounds, glm::vec2(width * 3.0f / 4.0f, height * 3.0f / 4.0f), 0.0f,
         glm::vec2(1.0f, 1.0f), region4.originCenter,
         region4.rotated ? Lucky::UVMode::RotatedCW90 : Lucky::UVMode::Normal, Lucky::Color::White);
 
-    batchRenderer->BatchQuad(nullptr, glm::vec2(windowWidth / 2.0f, windowHeight / 2.0f), 0.0f, glm::vec2(1.0f, 1.0f),
+    exampleData->batchRenderer->BatchQuad(nullptr, glm::vec2(width / 2.0f, height / 2.0f), 0.0f, glm::vec2(1.0f, 1.0f),
         glm::vec2(0.5f, 0.5f), Lucky::UVMode::Normal, Lucky::Color::White);
 
-    batchRenderer->End();
+    exampleData->batchRenderer->End();
 
-    graphicsDevice->EndFrame();
-    SDL_GL_SwapWindow(window);
+    gameData->graphicsDevice->EndFrame();
 
     return SDL_APP_CONTINUE;
 }
 
-void SDL_AppQuit(void *appstate, SDL_AppResult result)
+void SDLCALL LuckyQuit(Lucky::GameData *gameData)
 {
-    texture.reset();
-    textureAtlas.reset();
-    batchRenderer.reset();
-    graphicsDevice.reset();
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    ExampleData *exampleData = (ExampleData *)gameData->userData;
+
+    exampleData->texture.reset();
+    exampleData->textureAtlas.reset();
+    exampleData->batchRenderer.reset();
+    delete exampleData;
+
+    gameData->userData = nullptr;
 }
--- Source/Examples/04-Shaders.cpp
+++ Source/Examples/04-Shaders.cpp
@@ -1,102 +1,85 @@
 #include <memory>
-#include <stdint.h>
 
-#define _CRTDBG_MAP_ALLOC
-#include <crtdbg.h>
-
-#define SDL_MAIN_USE_CALLBACKS
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
-#include <spdlog/spdlog.h>
-
+#define LUCKY_MAIN
 #include <Lucky/Graphics/BatchRenderer.hpp>
 #include <Lucky/Graphics/Color.hpp>
-#include <Lucky/Graphics/GraphicsDevice.hpp>
 #include <Lucky/Graphics/ShaderProgram.hpp>
+#include <Lucky/Graphics/Texture.hpp>
+#include <Lucky/LuckyMain.hpp>
 
-const int windowWidth = 1920;
-const int windowHeight = 1080;
-
-SDL_Window *window;
-std::shared_ptr<Lucky::GraphicsDevice> graphicsDevice;
-std::unique_ptr<Lucky::BatchRenderer> batchRenderer;
-std::shared_ptr<Lucky::ShaderProgram> shader;
-uint64_t currentTime;
-float totalTime = 0;
-
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+struct ExampleData
 {
-    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+    std::unique_ptr<Lucky::BatchRenderer> batchRenderer;
+    std::shared_ptr<Lucky::ShaderProgram> shader;
+    double totalTime;
+};
 
-    SDL_SetAppMetadata("Shader Example", "1.0", "dev.jessechounard.shaders");
+void SDLCALL LuckyGetConfig(Lucky::ConfigData *configData)
+{
+    configData->screenWidth = 1920;
+    configData->screenHeight = 1080;
+    configData->windowTitle = "Shader Example";
+}
 
-    if (!SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_GAMEPAD))
-    {
-        spdlog::error("Failed to initialize SDL {}", SDL_GetError());
-        return SDL_APP_FAILURE;
-    }
+SDL_AppResult SDLCALL LuckyInitialize(Lucky::GameData *gameData)
+{
+    ExampleData *exampleData = new ExampleData();
 
-    auto attributes = Lucky::GraphicsDevice::PrepareWindowAttributes(Lucky::GraphicsAPI::OpenGL);
-
-    window = SDL_CreateWindow("Shader Example", windowWidth, windowHeight, attributes);
-    if (window == NULL)
-    {
-        spdlog::error("Failed to create window {}", SDL_GetError());
-        return SDL_APP_FAILURE;
-    }
-
-    graphicsDevice = std::make_shared<Lucky::GraphicsDevice>(
-        Lucky::GraphicsAPI::OpenGL, window, Lucky::VerticalSyncType::AdaptiveEnabled);
-    batchRenderer = std::make_unique<Lucky::BatchRenderer>(graphicsDevice, 1000);
+    exampleData->batchRenderer = std::make_unique<Lucky::BatchRenderer>(gameData->graphicsDevice, 1000);
 
     Lucky::VertexShader vertexShader("Basic.vert");
     Lucky::FragmentShader fragmentShader("ShaderArt.frag");
-    shader = std::make_shared<Lucky::ShaderProgram>(graphicsDevice, vertexShader, fragmentShader);
+    exampleData->shader =
+        std::make_shared<Lucky::ShaderProgram>(gameData->graphicsDevice, vertexShader, fragmentShader);
+    exampleData->totalTime = 0;
 
-    currentTime = SDL_GetPerformanceCounter();
+    gameData->userData = exampleData;
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDLCALL LuckyEvent(Lucky::GameData *gameData, SDL_Event *event, bool *eventHandled)
+{
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDLCALL LuckyUpdate(Lucky::GameData *gameData, double deltaSeconds)
+{
+    ExampleData *exampleData = (ExampleData *)gameData->userData;
+
+    exampleData->totalTime += deltaSeconds;
 
     return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+SDL_AppResult SDLCALL LuckyDraw(Lucky::GameData *gameData)
 {
-    if (event->type == SDL_EVENT_QUIT)
-    {
-        return SDL_APP_SUCCESS;
-    }
-    return SDL_APP_CONTINUE;
-}
+    ExampleData *exampleData = (ExampleData *)gameData->userData;
 
-SDL_AppResult SDL_AppIterate(void *appstate)
-{
-    uint64_t newTime = SDL_GetPerformanceCounter();
-    double frameTime = (newTime - currentTime) / (double)SDL_GetPerformanceFrequency();
+    gameData->graphicsDevice->BeginFrame();
+    gameData->graphicsDevice->ClearScreen(Lucky::Color::Black);
 
-    currentTime = newTime;
+    const int width = gameData->graphicsDevice->GetScreenWidth();
+    const int height = gameData->graphicsDevice->GetScreenHeight();
 
-    totalTime += (float)frameTime;
+    exampleData->batchRenderer->Begin(Lucky::BlendMode::None, nullptr, exampleData->shader);
+    exampleData->shader->SetParameter("iResolution", glm::vec2((float)width, (float)height));
+    exampleData->shader->SetParameter("iTime", (float)exampleData->totalTime);
+    exampleData->batchRenderer->BatchQuadUV(glm::vec2(0.0f, 0.0f), glm::vec2((float)width, (float)height),
+        glm::vec2(0.0, 0.0f), glm::vec2((float)width, (float)height), Lucky::Color::White);
+    exampleData->batchRenderer->End();
 
-    graphicsDevice->BeginFrame();
-    graphicsDevice->ClearScreen(Lucky::Color::Black);
-
-    batchRenderer->Begin(Lucky::BlendMode::None, nullptr, shader);
-    shader->SetParameter("iResolution", glm::vec2((float)windowWidth, (float)windowHeight));
-    shader->SetParameter("iTime", totalTime);
-    batchRenderer->BatchQuadUV(glm::vec2(0.0f, 0.0f), glm::vec2((float)windowWidth, (float)windowHeight),
-        glm::vec2(0.0, 0.0f), glm::vec2((float)windowWidth, (float)windowHeight), Lucky::Color::White);
-    batchRenderer->End();
-
-    graphicsDevice->EndFrame();
-    SDL_GL_SwapWindow(window);
+    gameData->graphicsDevice->EndFrame();
 
     return SDL_APP_CONTINUE;
 }
 
-void SDL_AppQuit(void *appstate, SDL_AppResult result)
+void SDLCALL LuckyQuit(Lucky::GameData *gameData)
 {
-    shader.reset();
-    batchRenderer.reset();
-    graphicsDevice.reset();
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    ExampleData *exampleData = (ExampleData *)gameData->userData;
+
+    exampleData->shader.reset();
+    exampleData->batchRenderer.reset();
+    delete exampleData;
+
+    gameData->userData = nullptr;
 }
